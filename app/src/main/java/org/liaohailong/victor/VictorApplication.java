78a4f94b1541b5a4579dea89d0c3489b4e010dc5
liaohailong <liaohailong190@foxmail.com>
2018-08-07 14:18:57 +0800

    修复上传文件，loaded方法回调两次的问题+1

--- app/src/main/java/org/liaohailong/victor/VictorApplication.java
+++ app/src/main/java/org/liaohailong/victor/VictorApplication.java
@@ -2,7 +2,6 @@ package org.liaohailong.victor;
 
 import android.app.Application;
 
-import com.squareup.leakcanary.LeakCanary;
 
 /**
  * Describe as : Victor APP
@@ -13,11 +12,5 @@ public class VictorApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        if (LeakCanary.isInAnalyzerProcess(this)) {
-            // This process is dedicated to LeakCanary for heap analysis.
-            // You should not init your app in this process.
-            return;
-        }
-        LeakCanary.install(this);
     }
 }
